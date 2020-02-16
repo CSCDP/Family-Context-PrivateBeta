b9bb0f4c8eebaaf96c90c9db4041df229cbc180c
Alexander Read <Alexander.Read@softwire.com>
2020-02-16 12:09:30 +0000

    Added react router

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,47 @@
 import React from 'react';
 import logo from './logo.svg';
 import './App.css';
+import { Link, Route } from 'react-router-dom';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+    <Route exact path="/" component={Home} />
+    <Route path="/alternate" component={Alternate} />
     </div>
   );
+}
+
+function Home(){
+  return (    <div className="App">
+  <header className="App-header">
+    <img src={logo} className="App-logo" alt="logo" />
+    <p>
+      Edit <code>src/App.tsx</code> and save to reload.
+    </p>
+    <Link to="/alternate">
+      Other page
+    </Link>
+  </header>
+</div>
+);
+}
+
+
+function Alternate(){
+  return (    
+  <div className="App">
+  <header className="App-header">
+    <img src={logo} className="App-logo" alt="logo" />
+    <p>
+      Alternate page reached through react router.
+    </p>
+    <Link to="/">
+      Back to home
+    </Link>
+  </header>
+</div>
+);
 }
 
 export default App;
