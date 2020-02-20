2b83098b101d64dc0106b4831592fa972ea50999
Lou <louisa.nash@softwire.com>
2020-02-20 11:10:22 +0000

    Basic implementation of accordion

--- src/components/SmallAccordion.tsx
+++ src/components/SmallAccordion.tsx
@@ -1,4 +1,6 @@
 import React from 'react';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import './SmallAccordion.css';
 import Accordion from 'react-bootstrap/Accordion';
 import Card from 'react-bootstrap/Card';
 import AccordionCollapse from 'react-bootstrap/AccordionCollapse';
@@ -11,13 +13,20 @@ const SmallAccordion: React.FC<any> = (props: any) => {
     return (
         <Accordion>
             <Card>
-                <Accordion.Toggle as={Card.Header} eventKey='0'><span>&#43;</span></Accordion.Toggle>
+                <div>
+                <div className="govuk-grid-column-three-quarters">
+                <h3 className="govuk-heading-m govuk-!-width-one-half govuk-!-margin-top-2">govuk-heading-m</h3>
+                <p className="govuk-body-s">Information is correct as of <b>04/11/2019 at 23:59</b></p>
+                </div>
+                <div className="govuk-grid-column-one-quarter">
+                <Accordion.Toggle className="toggle" as={Card.Header} eventKey='0'><span>&#43;</span></Accordion.Toggle>
+                </div>
+                </div>
                 <AccordionCollapse eventKey='0'>
                     <Card.Body>
-                        thingy
-                        {/* <Table>
+                        <Table>
                             <TableBody rowList={rowValues}></TableBody>
-                        </Table> */}
+                        </Table> 
                     </Card.Body>
                 </AccordionCollapse>
             </Card>
