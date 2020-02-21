ec0823cecd606a682e170c9d2dc8707efc5082d5
Lou <louisa.nash@softwire.com>
2020-02-21 10:24:57 +0000

    Format current master branch

--- src/App.tsx
+++ src/App.tsx
@@ -3,18 +3,15 @@ import './App.css';
 import { BrowserRouter as Router, Route, Link } from 'react-router-dom';
 import FamilyContextHeader from './components/FamilyContextHeader';
 import IndividualPage from './pages/IndividualPage';
+import GovSpacing from './components/GovSpacing';
 
 const App: React.FC = () => {
   return (
     <Router>
       <FamilyContextHeader />
-      <div className="govuk-width-container">
-        <main className="govuk-main-wrapper">
-
+        <GovSpacing>
           <Route exact path="/" component={IndividualPage} />
-
-        </main>
-      </div>
+          </GovSpacing>
     </Router>
   );
 }
