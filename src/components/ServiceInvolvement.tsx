777fe11e13609ef145939ad8bdbf55bf8ecf4002
Lou <louisa.nash@softwire.com>
2020-02-26 11:24:02 +0000

    work in progress adding state

--- src/components/ServiceInvolvement.tsx
+++ src/components/ServiceInvolvement.tsx
@@ -7,11 +7,19 @@ import Table from './Table/Table';
 import TableBody from './Table/TableBody';
 import TitleValuePairTableRow from './Table/TitleValuePairTableRow';
 
-class ServiceInvolvement extends React.Component<{ personDetails: PersonDetails }, { open: boolean }> {
+interface dictionary {
+    [id: string] : TitleValuePair[]
+}
 
-    constructor(props: { personDetails: PersonDetails }) {
+class ServiceInvolvement extends React.Component<{ serviceInvolvementDetailsSummaries: ServiceInvolvementDetailsSummary[] }, { serviceInvolvementDetailsData: dictionary }> {
+
+    constructor(props: { serviceInvolvementDetailsSummaries: ServiceInvolvementDetailsSummary[] }) {
         super(props);
-        this.state = { open: false };
+        this.state = { serviceInvolvementDetailsData: {} };
+        this.props.serviceInvolvementDetailsSummaries.map(sid => {
+            this.setState({ ...this.state.serviceInvolvementDetailsData, sid.id = [] });
+        });
+        
     }
 
     click(newOpen: boolean): void {
@@ -24,29 +32,24 @@ class ServiceInvolvement extends React.Component<{ personDetails: PersonDetails 
     }
 
     render() {
-
-        if (this.props.personDetails.id > 0) {
             let serviceInvolvementDetailsSummary: ServiceInvolvementDetailsSummary = { title: "Police", coverageStartDate: "31/10/2017", coverageEndDate: "04/11/2019", recordsAvailable: true, id: "1", lastSynchronized: "04/11/2019" }
             let content: TitleValuePair[] = [{ title: "Service Involvement", value: "Current" }, { title: "Police Contact", value: "Jason Davies" }, { title: "Lead practitioner contact", value: "" }, { title: "Last offence", value: "" }, { title: "Date of offence", value: "" }, { title: "Type of offence", value: "Domestic abuse" }, { title: "Nature of involvement", value: "Victim" }]
             return (
                 <div id="service-involvements">
                     <div className="govuk-accordion js-enabled" data-module="govuk-accordion" id="accordion-with-summary-sections">
-                        
-                        <ServiceInvolvementAccordion serviceInvolvementDetailsSummary={serviceInvolvementDetailsSummary} click={() => this.click(!this.state.open)}>
+                    {this.props.serviceInvolvementDetailsSummaries.map(sids=>
+                        <ServiceInvolvementAccordion serviceInvolvementDetailsSummary={sids} click={() => this.click(!this.state.open)}>
                             <Table>
                                 <TableBody>
-                                    {content.map(element => (
+                                    {this.state.thing[sids.id] ? content.map(element => (
                                         <TitleValuePairTableRow rowTitle={element.title} rowValue={element.value} format="govuk-!-font-size-14" />
-                                    ))}
+                                    )) : null}
                                 </TableBody>
                             </Table>
-                        </ServiceInvolvementAccordion>
+                        </ServiceInvolvementAccordion>)}
                     </div>
                 </div>
             )
-        } else {
-            return null;
-        }
 
     }
 }
