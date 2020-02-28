6156f123fe19820c7098d8c45ac89764bc5ef906
Lou <louisa.nash@softwire.com>
2020-02-28 12:13:04 +0000

    Revert "FAM-45: Add no records available card"
    This reverts commit 840fb13fe4cb7f8ce49550634377e1af581c5311.

--- src/components/Accordion/ServiceInvolvementAccordion.tsx
+++ src/components/Accordion/ServiceInvolvementAccordion.tsx
@@ -5,31 +5,20 @@ import AccordionSection from './AccordionSection';
 import AccordionSectionHeader from './AccordionSectionHeader';
 import AccordionContent from './AccordionContent';
 import ServiceInvolvementDetailsSummary from '../../models/ServiceInvolvementDetailsSummary';
-import CardHeader from '../CardHeader';
 
 const ServiceInvolvementAccordion: React.SFC<{ serviceInvolvementDetailsSummary: ServiceInvolvementDetailsSummary, click: () => void, children: any }> = (props: { serviceInvolvementDetailsSummary: ServiceInvolvementDetailsSummary, click: () => void, children: any }) => {
 
-  if (props.serviceInvolvementDetailsSummary.recordsAvailable) {
     return (
       <AccordionSection>
-        <AccordionSectionHeader click={() => props.click()}>
-          <AccordionHeader title={props.serviceInvolvementDetailsSummary.title} ></AccordionHeader>
-          <ServiceInvolvementAccordionSummary serviceInvolvement={props.serviceInvolvementDetailsSummary} />
-        </AccordionSectionHeader>
-        <AccordionContent>
-          {props.children}
-        </AccordionContent>
-      </AccordionSection>
-    );
-  }
-  else {
-    return (
-      <div className="CardHeader">
-        <CardHeader title={props.serviceInvolvementDetailsSummary.title} ></CardHeader>
+      <AccordionSectionHeader click={() => props.click()}>
+        <AccordionHeader title={props.serviceInvolvementDetailsSummary.title} ></AccordionHeader>
         <ServiceInvolvementAccordionSummary serviceInvolvement={props.serviceInvolvementDetailsSummary} />
-      </div>
+      </AccordionSectionHeader>
+      <AccordionContent>
+        {props.children}
+      </AccordionContent>
+    </AccordionSection>
     )
-  }
 }
 
 export default ServiceInvolvementAccordion;
