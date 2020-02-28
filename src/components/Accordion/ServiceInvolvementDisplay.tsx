466f4c8fcd1c5f705b54f84a6eb5b0ea6db5d2b1
Lou <louisa.nash@softwire.com>
2020-02-28 10:49:38 +0000

    FAM-56: format service detail component better

--- src/components/Accordion/ServiceInvolvementDisplay.tsx
+++ src/components/Accordion/ServiceInvolvementDisplay.tsx
@@ -6,50 +6,60 @@ import TableHeader from "../Table/TableHeader";
 const ServiceInvolvementDisplay: React.SFC<{ details: ServiceDetail | null }> = (props: { details: ServiceDetail | null }) => {
 
     let properties = props.details?.schema.properties;
-    let results: any[] = []; 
+    let results: any[] = [];
     for (let key in properties) {
         let value = properties[key];
-        results[value["x-item-seq"]-1] = getData(key, value, props.details?.data);
+        results[value["x-item-seq"] - 1] = getData(key, value, props.details?.data);
     }
 
-    function getData(key: string, value: { [id: string]: any}, data?: { [id: string]: any}, index?: number) {
+    function getData(key: string, value: { [id: string]: any }, data?: { [id: string]: any }, index?: number) {
         if (data) {
             if (value["type"] == "string") {
-                let title = value["title"] ? value["title"] : key;
-                let result;
-                index==undefined ? result = <TitleValuePairTableRow rowTitle={title} rowValue={data[key]} format="govuk-!-font-size-14" /> : result = <TitleValuePairTableRow rowTitle={title+" "+index} rowValue={data[key]} format="govuk-!-font-size-14" />;
-                return result;
+                return getStringData(key, value, data, index);
             } else if (value["type"] == "array") {
-                let title = value["title"] ? value["title"] : key;
-                let minitable = value["items"]["properties"];
-                let results: any[] = [];
-                results[0] = <th>{title}</th>
-                let index = 1;
-                for (let dataKey in data[key]) {
-                    let dataItem = data[key][dataKey];
-                    let miniresults: any[] = [];
-                    for (let minikey in minitable) {
-                        let minivalue = minitable[minikey];
-                        miniresults[minivalue["x-item-seq"]] = getData(minikey, minivalue, dataItem, index);
-                    }
-                    results[index] = miniresults;
-                    index++;
-                }
-                return results;
+                return getArrayData(key, value, data);
             } else if (value["type"] == "object") {
-                let title = value["title"] ? value["title"] : key;
-                let minitable = value["properties"];
-                let miniresults: any[] = [];
-                miniresults[0] = <th>{title}</th>
-                for (let minikey in minitable) {
-                    let minivalue = minitable[minikey];
-                    miniresults[minivalue["x-item-seq"]] = getData(minikey, minivalue, data[key]);
-                }
-                return miniresults;
+                return getObjectData(key, value, data, index);
             }
         }
     }
-    
+
+    function getStringData(key: string, value: { [id: string]: any }, data: { [id: string]: any }, index?: number) {
+        let title = value["title"] ? value["title"] : key;
+        let result;
+        index == undefined ? result = <TitleValuePairTableRow rowTitle={title} rowValue={data[key]} format="govuk-!-font-size-14" /> : result = <TitleValuePairTableRow rowTitle={title + " " + index} rowValue={data[key]} format="govuk-!-font-size-14" />;
+        return result;
+    }
+    function getArrayData(key: string, value: { [id: string]: any }, data: { [id: string]: any }) {
+        let title = value["title"] ? value["title"] : key;
+        let table = value["items"]["properties"];
+        let results: any[] = [];
+        results[0] = <th>{title}</th>
+        let internalIndex = 1;
+        for (let dataKey in data[key]) {
+            let dataItem = data[key][dataKey];
+            results[internalIndex] = getSmallTableData(table, dataItem, internalIndex);
+            internalIndex++;
+        }
+        return results;
+    }
+    function getObjectData(key: string, value: { [id: string]: any }, data: { [id: string]: any }, index?: number) {
+        let title = value["title"] ? value["title"] : key;
+        let table = value["properties"];
+        let results = getSmallTableData(table, data[key], index);
+        results[0] = <th>{title}</th>
+        return results;
+    }
+
+    function getSmallTableData(table: { [id: string]: any }, data: { [id: string]: any }, index?: number) {
+        let results: any[] = [];
+        for (let key in table) {
+            let value = table[key];
+            results[value["x-item-seq"]] = getData(key, value, data, index);
+        }
+        return results;
+    }
+
     return results.length > 0 ? <div>{results}</div> : null;
 }
 
