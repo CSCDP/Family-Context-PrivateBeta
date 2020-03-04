a88e7c51ad494153384cb0a8313ee2ee8dd4c363
Lou <louisa.nash@softwire.com>
2020-03-04 08:40:02 +0000

    FAM-63: styling of page started

--- src/components/SearchPage/CaseIdSearch.tsx
+++ src/components/SearchPage/CaseIdSearch.tsx
@@ -9,20 +9,22 @@ type CaseIdSearchProps = {
 const CaseIdSearch: React.FC<CaseIdSearchProps> = (props) => {
     var caseId = "";
 
-    var search = (event: React.MouseEvent<HTMLButtonElement, MouseEvent>) : void => {
+    var search = (event: React.MouseEvent<HTMLButtonElement, MouseEvent>): void => {
         event.preventDefault();
         props.search(caseId);
     }
 
     return (
         <div className="CaseIdSearch">
-            <div className="govuk-hint">
-                To view a child with a known case, enter case ID below:
+            <div className="govuk-grid-column-one-half">
+                <div className="govuk-hint">
+                    To view a child with a known case, enter case ID below:
             </div>
-            <TextInputGroup onChange={(text: string) => caseId = text} id="caseId" name="Case ID"/>
-            <button className="govuk-button" data-module="govuk-button" onClick={(event) => search(event)}>
-                View child details
+                <TextInputGroup onChange={(text: string) => caseId = text} id="caseId" name="Case ID" />
+                <button className="govuk-button" data-module="govuk-button" onClick={(event) => search(event)}>
+                    View child details
             </button>
+            </div>
         </div>
     )
 }
--- src/components/SearchPage/InfoSearch.tsx
+++ src/components/SearchPage/InfoSearch.tsx
@@ -2,7 +2,7 @@ import React from "react";
 import { TextInputGroup } from "../InputGroups";
 
 type InfoSearchProps = {
-    search: (info: {[id: string]: string}) => void
+    search: (info: { [id: string]: string }) => void
 }
 
 const InfoSearch: React.FC<InfoSearchProps> = (props) => {
@@ -12,26 +12,33 @@ const InfoSearch: React.FC<InfoSearchProps> = (props) => {
     var month = "";
     var year = "";
 
-    var search = (event: React.MouseEvent<HTMLButtonElement, MouseEvent>) : void => {
+    var search = (event: React.MouseEvent<HTMLButtonElement, MouseEvent>): void => {
         event.preventDefault();
-        let dob = day != "" && month != "" && year != "" ? month+"%2F"+day+"%2F"+year : "";
-        props.search({firstName: firstName, familyName: familyName, dob: dob});
+        let dob = day != "" && month != "" && year != "" ? month + "%2F" + day + "%2F" + year : "";
+        props.search({ firstName: firstName, familyName: familyName, dob: dob });
     }
 
     return (
-        <div className="NameSearch">
-            <TextInputGroup onChange={(text: string) => firstName = text} id="firstName" name="First Name"/>
-
-            <TextInputGroup onChange={(text: string) => familyName = text} id="familyName" name="Family Name"/>
-            <div className="govuk-hint">
-                Date of birthday (optional)
+        <div className="InfoSearch">
+            <div className="govuk-grid-column-one-half">
+                <TextInputGroup onChange={(text: string) => firstName = text} id="firstName" name="First Name" />
+                <TextInputGroup onChange={(text: string) => familyName = text} id="familyName" name="Family Name" />
+                <div className="govuk-hint">
+                    Date of birthday (optional)
+                </div>
+                <div className="govuk-grid-column-one-third">
+                    <TextInputGroup onChange={(text: string) => day = text} id="day" name="Day" />
+                </div>
+                <div className="govuk-grid-column-one-third">
+                    <TextInputGroup onChange={(text: string) => month = text} id="month" name="Month" />
+                </div>
+                <div className="govuk-grid-column-one-third">
+                    <TextInputGroup onChange={(text: string) => year = text} id="year" name="Year" />
+                </div>
+                <button className="govuk-button" data-module="govuk-button" onClick={(event) => search(event)}>
+                    Search
+                </button>
             </div>
-            <TextInputGroup onChange={(text: string) => day = text} id="day" name="Day"/>
-            <TextInputGroup onChange={(text: string) => month = text} id="month" name="Month"/>
-            <TextInputGroup onChange={(text: string) => year = text} id="year" name="Year"/>
-            <button className="govuk-button" data-module="govuk-button" onClick={(event) => search(event)}>
-                Search
-            </button>
         </div>
     )
 }
