357e8e426dd69887cccbdbb655b4e6d61d3a162b
Alexander Read <Alexander.Read@softwire.com>
2020-03-20 12:09:17 +0000

    FAM-NONE: Minimise changes to array component

--- src/components/ServiceInvolvementContent/ArrayComponent.tsx
+++ src/components/ServiceInvolvementContent/ArrayComponent.tsx
@@ -1,5 +1,5 @@
 import React from 'react'
-import { ArraySchema, ObjectSchema } from '../../models/Schema';
+import { ArraySchema } from '../../models/Schema';
 import GenericComponent from './GenericComponent';
 
 const ArrayComponent: React.FC<{ data: any[], schema: ArraySchema, keyId: string }> = (props: { data: any[], schema: ArraySchema, keyId: string }) => {
@@ -7,20 +7,18 @@ const ArrayComponent: React.FC<{ data: any[], schema: ArraySchema, keyId: string
     if (props.data == null) {
         return <></>
     }
-   
+
     let arrayKeyRoot = props.schema.title ?? props.keyId;
     let elementArray = props.data.map((dataItem, index) =>
-        {
-            return (
-                <GenericComponent schema={props.schema.items} data={dataItem} keyId={""} key={index} arrayIndex={" " + ++index}/>
-            )
-        });
+        (
+            <GenericComponent schema={props.schema.items} data={dataItem} keyId={""} key={index} arrayIndex={" " + ++index} />
+        ));
 
     return <>
-        <br/>
+        <br />
         <p className="govuk-!-font-weight-bold govuk-!-font-size-16">{arrayKeyRoot}</p>
         {elementArray}
-        </>;
+    </>;
 }
 
 export default ArrayComponent;
